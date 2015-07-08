4b3ebf12c5847b021a3559ed12bd272fd57ac3b9
peter-edge <peter.edge@gmail.com>
2015-07-08 11:13:34 -0700

    cleanup

--- cacerts.go
+++ cacerts.go
@@ -1,6 +1,8 @@
 /*
 Package cacerts contains CA certificates from the golang:1.4.2 docker image at /etc/ssl/certs/ca-certificates.crt.
+
 Useful for when docker images are created from the scratch image.
+
 See https://medium.com/@kelseyhightower/optimizing-docker-images-for-static-binaries-b5696e26eb07 for details.
 */
 package cacerts
